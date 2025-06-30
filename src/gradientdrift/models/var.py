3260c53a841642264ffa4f8c5ffb58d3d5811b3f
philippe554 <philippe.mmd@gmail.com>
2025-06-30 14:30:43 +0200

    Add closed form solution for a VAR

--- src/gradientdrift/models/var.py
+++ src/gradientdrift/models/var.py
@@ -8,6 +8,18 @@ class VAR(Model):
         self.numberOfLags = numberOfLags
         self.numberOfVariables = numberOfVariables
 
+        self.params = {
+            'coeffs': jax.numpy.zeros((self.numberOfLags, self.numberOfVariables, self.numberOfVariables)),
+            'const': jax.numpy.zeros((self.numberOfVariables,)),
+            'logSigma': jax.numpy.zeros((self.numberOfVariables,))
+        }
+
+        self.paramDims = {
+            'coeffs': ["L[i+1]", "VAR[i]", "EQ[i]"],
+            'const': ["EQ[i]"],
+            'logSigma': ["EQ[i]"]
+        }
+
     def requestPadding(self, dataset):
         dataset.setLeftPadding(self.numberOfLags)
         dataset.setRightPadding(0)
@@ -79,9 +91,6 @@ class VAR(Model):
         x = data[:-1, :]
         y = data[self.numberOfLags:, :]
 
-        if x.shape[0] != y.shape[0]:
-            raise ValueError("Input x and y must have the same number of rows after padding.")
-
         yHat = self.predict(params, x)
 
         residuals = y - yHat
@@ -91,3 +100,60 @@ class VAR(Model):
         log_prob = jax.scipy.stats.norm.logpdf(residuals, scale=sigma).sum(axis=1)
         
         return jax.numpy.mean(log_prob)
+
+    def fitClosedForm(self, dataset, batchSize = 100):
+
+        # TODO: Check transpositions of matrices
+        # TODO: add JIT
+
+        self.fitConfig = {
+            "Method": "Closed Form",
+            "Batch size": "Full batch" if batchSize == -1 else batchSize,
+        }
+
+        self.requestPadding(dataset)
+        dataset.prepareBatches(batchSize)
+        numberOfBatches = dataset.getNumberOfBatches()
+
+        self.fitConfig['Number of batches'] = numberOfBatches
+
+        numberIndependentVariables = self.numberOfLags * self.numberOfVariables + 1  # +1 for the constant term
+        XXSum = jax.numpy.zeros((self.numberOfVariables, numberIndependentVariables, numberIndependentVariables))
+        XYSum = jax.numpy.zeros((self.numberOfVariables, numberIndependentVariables, 1))
+
+        for i in range(numberOfBatches):
+            batch = dataset.getBatch(i)
+            samplesInBatch = batch.data.shape[0] - self.numberOfLags
+
+            # X is the same for every equation, so we can compute it once
+            X = jax.numpy.ones((samplesInBatch, self.numberOfLags * self.numberOfVariables + 1))
+
+            for lag in range(self.numberOfLags):
+                X = X.at[:, 1 + lag * self.numberOfVariables : 1 + (1 + lag) * self.numberOfVariables].set(
+                    batch.data[self.numberOfLags - 1 - lag:samplesInBatch + self.numberOfLags - 1 - lag, :])
+
+            Y = batch.data[self.numberOfLags:, :].T.reshape(self.numberOfVariables, samplesInBatch, 1)
+
+            XXSum += X.T @ X
+            XYSum += X.T @ Y
+
+        B = jax.numpy.linalg.solve(XXSum, XYSum)
+
+        self.params['const'] = B[:, 0].reshape(self.numberOfVariables)
+        self.params['coeffs'] = B[:, 1:].T.reshape(self.numberOfLags, self.numberOfVariables, self.numberOfVariables)
+
+        errorSumOfSquares = jax.numpy.zeros((self.numberOfVariables, self.numberOfVariables))
+
+        for i in range(numberOfBatches):
+            batch = dataset.getBatch(i)
+            x = batch.data[:-1, :]
+            y = batch.data[self.numberOfLags:, :]
+            yHat = self.predict(self.params, x)
+            residuals = y - yHat
+            errorSumOfSquares += residuals.T @ residuals
+
+        sigma = jax.numpy.sqrt(jax.numpy.diag(errorSumOfSquares) / (dataset.getEffectiveNObs() - numberIndependentVariables))
+        epsilon = 1e-9
+        self.params['logSigma'] = jax.numpy.log(jax.numpy.exp(sigma) - 1 + epsilon)
+
+
